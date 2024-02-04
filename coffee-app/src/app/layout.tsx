6d53c0174f4324aef124e57980c14d009e5ed79f
TaiShan  Lin <taishanlin@TaiShans-MBP.ht.home>
2024-02-04 11:33:01 -0500

    updated layout

--- coffee-app/src/app/layout.tsx
+++ coffee-app/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import CustomCursor from '@/components/cursor'
+import HomePage from './page'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,7 +15,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className={inter.className}>
-        {children} <CustomCursor />
+        <HomePage />
+        <CustomCursor />
       </body>
     </html>
   )
--- coffee-app/src/app/page.tsx
+++ coffee-app/src/app/page.tsx
@@ -3,7 +3,7 @@ import { FilterContainer } from '@/components/filter/container'
 import { coffeeList } from '../lib/constants/coffee-list'
 import { filterCategories } from '@/lib/constants/filter-categories'
 
-function HomePage() {
+export default function HomePage() {
   const tabs: Breadcrumb[] = [
     { label: 'Home', href: '/' },
     { label: 'Features', href: '/product' },
@@ -17,5 +17,3 @@ function HomePage() {
     </div>
   )
 }
-
-export default HomePage
